4a089c35194152ca9535d04a112c1f91fc987cfa
Struggler <22187936+changedbalram@users.noreply.github.com>
2020-11-28 16:03:20 +0530

    Zipkin and Sleuth added
    http://127.0.0.1:9411/zipkin/traces/

--- department-service/src/main/java/com/balramc/department/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/balramc/department/DepartmentServiceApplication.java
@@ -2,12 +2,14 @@ package com.balramc.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author balram
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/balramc/user/UserServiceApplication.java
+++ user-service/src/main/java/com/balramc/user/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.balramc.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author balram
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
