b3050b681fb949ee63768161d955c8beb65d1b12
Struggler <22187936+changedbalram@users.noreply.github.com>
2021-01-03 19:01:19 +0530

    Update

--- cloud-api-gateway/src/main/java/com/balramc/cloud/api/gateway/controller/FallBackController.java
+++ cloud-api-gateway/src/main/java/com/balramc/cloud/api/gateway/controller/FallBackController.java
@@ -12,14 +12,14 @@ public class FallBackController {
 	@GetMapping(path = "/userServiceFallBack")
 	public Map<String, String> userServiceFallBack() {
 		HashMap<String, String> hashMap = new HashMap<>();
-		hashMap.put("fallback","User service is taking longer than usual please try after some time");
+		hashMap.put("fallback","User service is taking longer than usual please try after some time...");
 		return hashMap;
 	}
 	
 	@GetMapping(path = "/departmentServiceFallBack")
 	public Map<String, String> departmentServiceFallBack() {
 		HashMap<String, String> hashMap = new HashMap<>();
-		hashMap.put("fallback","Department service is taking longer than usual please try after some time");
+		hashMap.put("fallback","Department service is taking longer than usual please try after some time...");
 		return hashMap;
 	}
 }
