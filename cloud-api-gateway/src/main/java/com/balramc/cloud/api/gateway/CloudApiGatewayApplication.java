9ca1910afeaba99f67463ecac0bfa41b82910084
cheryalabalram <cheryalabalram@yahoo.com>
2025-10-12 19:34:39 +0530

    Spring boot v3.4.10 updated

--- cloud-api-gateway/src/main/java/com/balramc/cloud/api/gateway/CloudApiGatewayApplication.java
+++ cloud-api-gateway/src/main/java/com/balramc/cloud/api/gateway/CloudApiGatewayApplication.java
@@ -2,12 +2,10 @@ package com.balramc.cloud.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableHystrix
+@EnableDiscoveryClient
 public class CloudApiGatewayApplication {
 
 	public static void main(String[] args) {
